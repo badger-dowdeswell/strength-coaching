745290061065a24d0531a8dbaa10980ad2cdfc33
badger-dowdeswell <badger@rockweather.com>
2025-08-02 07:31:24 +1200

    Saturday sync

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -1,9 +1,18 @@
 //
-// MORE PAGE
-// =========
-// This is the Strength Coaching Online public Tell me more page where users can read
-// information about the site on multiple pages. They cannot proceed further 
-// without logging in and authenticating their identity or registering.
+// MORE INFORMATION PAGES
+// ======================
+// These are the Strength Coaching Online public "Tell Me More..." pages where anyone 
+// can read information about the company and what we do on multiple pages, structured
+// pages. 
+//
+// Documentation
+// =============
+// These page do not require the users to be registered or authenticated. However,
+// they cannot proceed further without logging in and authenticating their identity
+// or registering.
+//
+// This is a multi-page component that switches between visible sections by incrementing
+// or decrementing the pageNumber useState hook.
 //
 // Revision History
 // ================
@@ -34,7 +43,6 @@ function More() {
     sessionStorage.setItem("JWT", ""); 
 
     let navigate = useNavigate();
-
     const [pageNumber, setPageNumber] = useState(1);
 
     return (        
@@ -66,13 +74,22 @@ function More() {
                     setPageNumber={setPageNumber}
                     navigate={navigate}
                 />
-            )};         
+            )};    
+
+            {(pageNumber === 4) && (
+                <Page_4
+                    setPageNumber={setPageNumber}
+                    navigate={navigate}
+                />
+            )};       
         </div>    
     );
 }   
 //
 // Page_1
 // ======
+// This is the Luke here.. introductory page.
+//
 function Page_1(params) {    
     return (        
         <div>                         
@@ -87,11 +104,11 @@ function Page_1(params) {
                         src={Luke_gym}
                         alt="/"
                         draggable={false}                            
-                        width={455}
+                        width={525}
                     />             
 
                     <div className="flex flex-col">                                                               
-                        <p className="ml-5 mr-5 text-white text-5xl font-bold">
+                        <p className="ml-5 mr-5 text-white text-6xl font-bold">
                             Luke here                      
                         </p>
                         <br></br>
@@ -99,7 +116,7 @@ function Page_1(params) {
                             I am all about helping                      
                         </p>
                         <p className="ml-5 mr-5 text-white text-2xl font-bold">
-                            YOU get strong !                      
+                            <span className="text-cyan-300 text-2xl font-bold">YOU</span> get strong !                     
                         </p>
                         <br></br>
                         <p className="ml-5 mr-5 text-white text-sm">
@@ -121,6 +138,7 @@ function Page_1(params) {
                         <p className="ml-5 mr-5 text-white text-sm">
                             YOU get strong.                
                         </p>
+                        <br></br>
 
                         <div className="flex flex-row">
                             <button className="bg-cyan-600 text-white font-bold text-sm py-2 px-2 rounded
@@ -138,7 +156,7 @@ function Page_1(params) {
                                     onClick={() => {
                                         params.setPageNumber(2);
                                     }}>
-                                More &gt; 
+                                Next &gt; 
                             </button>
                         </div>
                     </div>                     
@@ -151,6 +169,8 @@ function Page_1(params) {
 //
 // Page_2
 // ======
+// This is the "Train like the Pros" page.
+
 function Page_2(params) {    
     return (        
         <div>                         
@@ -163,8 +183,7 @@ function Page_2(params) {
 
                     <div>
                         <p className="ml-10 text-white text-5xl font-bold">
-                            Train like the 
-                            <span className="text-cyan-300 text-5xl font-bold"> Pros</span>                            
+                            Train like the <span className="text-cyan-300 text-5xl font-bold"> Pros</span>                            
                         </p>
                         <br></br>
                         
@@ -184,7 +203,7 @@ function Page_2(params) {
                     </div>
 
                     <div className="flex flex-col"> 
-                        <img className="rounded ml-24"
+                        <img className="ml-24 box-border border-2 rounded-lg"
                              src={laptop}
                              alt="/"
                              draggable={false}                            
@@ -240,7 +259,7 @@ function Page_2(params) {
                                     id="More"
                                     style={{ width: "100px"}}
                                     onClick={() => {params.setPageNumber(3);}}>
-                                More &gt; 
+                                Next &gt; 
                             </button>
                         </div>
                     </div>                     
@@ -252,6 +271,8 @@ function Page_2(params) {
 //
 // Page_3
 // ======
+// This is the "How does this work?" page.
+//
 function Page_3(params) {    
     return (        
         <div>                         
@@ -266,7 +287,7 @@ function Page_3(params) {
                         src={Luke_gym_2} 
                         alt="/"
                         draggable={false}                            
-                        width={355}
+                        width={310}
                     />             
 
                     <div className="flex flex-col">                                                               
@@ -277,11 +298,11 @@ function Page_3(params) {
 
                         <div>
                             <ul>
-                                <li className="ml-5 mr-5 mb-2 text-white text-2xl font-bold">&#8226; A Dynamic Program. </li>
-                                <li className="ml-5 mr-5 mb-2 text-white text-2xl font-bold">&#8226; Custom Analytic Reports.</li>
-                                <li className="ml-5 mr-5 mb-2 text-white text-2xl font-bold">&#8226; In-depth Video Analysis.</li>
-                                <li className="ml-5 mr-5 mb-0 text-white text-2xl font-bold">&#8226; Education about the WHY,</li>
-                                <p className="ml-5 mr-5 text-white text-2xl font-bold">&nbsp; &nbsp;not only the WHAT.</p>
+                                <li className="ml-5 mr-5 mb-2 text-white text-2xl font-bold">&#8226;&nbsp; A Dynamic Program. </li>
+                                <li className="ml-5 mr-5 mb-2 text-white text-2xl font-bold">&#8226;&nbsp; Custom Analytic Reports.</li>
+                                <li className="ml-5 mr-5 mb-2 text-white text-2xl font-bold">&#8226;&nbsp; In-depth Video Analysis.</li>
+                                <li className="ml-5 mr-5 mb-0 text-white text-2xl font-bold">&#8226;&nbsp; Education about the WHY,</li>
+                                <p className="ml-10 mr-5 text-white text-2xl font-bold">not only the WHAT.</p>
                             </ul>
                         </div>
                         <br></br>
@@ -290,7 +311,7 @@ function Page_3(params) {
                             Do you want Free Programs, Ebooks,                   
                         </p>  
                         <p className="ml-5 mr-5 text-white text-2xl font-bold">
-                            and other resouces?                  
+                            and other resouces ?                  
                         </p>  
                         <br></br>
 
@@ -319,8 +340,8 @@ function Page_3(params) {
                                                                 mt-2 ml-5"
                                     id="More"
                                     style={{ width: "100px"}}
-                                    onClick={() => {params.setPageNumber(3)}}>
-                                More &gt; 
+                                    onClick={() => {params.setPageNumber(4)}}>
+                                Next &gt; 
                             </button>
                         </div>
                     </div>                     
@@ -329,5 +350,100 @@ function Page_3(params) {
         </div>         
     );
 }
+
+//
+// Page_4
+// ======
+// This is the "What the strong people have to say" page.
+//
+function Page_4(params) {    
+    return (        
+        <div>                         
+            <TopNav title="" userName="" userRole=""/>
+
+            <div className="flex flex-col">
+                <div className="flex flex-row absolute top-24 bottom-0
+                                items-center justify-center
+                                left-0 right-0 bg-gray-800 overflow-hidden">
+                    <div className="flex flex-col">
+                        <p className="ml-5 mr-5 text-white text-3xl font-bold">
+                            What the strong people have to say !                  
+                        </p>
+                        <br></br>
+
+                        <p className="ml-5 mr-5 text-white text-3xl font-bold">
+                            next bit          
+                        </p>                     
+
+                        <div className="flex flex-row ml-14">
+                            <button className="bg-cyan-600 text-white font-bold text-sm py-2 px-2 rounded
+                                                mt-2 ml-5"
+                                    id="More"
+                                    style={{ width: "100px"}}
+                                    onClick={() => {params.setPageNumber(3);}}>                                 
+                                &lt; Back
+                            </button>
+
+                            <button className="bg-cyan-600 text-white font-bold text-sm py-2 px-2 rounded
+                                                mt-2 ml-5"
+                                    id="Free"
+                                    style={{ width: "140px"}}
+                                    onClick={() => params.setPageNumber(1)}>                                    
+                                Load More
+                            </button>
+
+                            <button className="bg-cyan-600 text-white font-bold text-sm py-2 px-2 rounded
+                                                                mt-2 ml-5"
+                                    id="Next"
+                                    style={{ width: "100px"}}
+                                    onClick={() => {params.setPageNumber(3);}}>
+                                Next &gt; 
+                            </button>
+                        </div>  
+                    </div>
+                </div> 
+            </div>
+        </div>    
+    );
+}
+
+
+
+        //         <div className="flex flex-row absolute top-24 bottom-0
+        //                         items-center justify-center
+        //                         left-0 right-0 bg-gray-800 overflow-hidden">
+
+        //             <div className="flex flex-col">
+        //                 <p className="ml-5 mr-5 text-white text-3xl font-bold">
+        //                     Left t                 
+        //                 </p>
+
+        //                 <p className="ml-5 mr-5 text-white text-3xl font-bold">
+        //                     Right r                 
+        //                 </p>
+        //             </div>
+                                                                            
+                    
+        //             <br></br>
+
+        //             <div className="flex flex-col">
+        //                 <div>
+        //                     <p className="ml-5 mr-5 text-white text-3xl font-bold">
+        //                         Left                  
+        //                     </p>
+
+        //                     <p className="ml-5 mr-5 text-white text-3xl font-bold">
+        //                         Right                  
+        //                     </p>
+
+        //                 </div>
+                        
+        //             </div> 
+
+
+                    
+        //         </div>
+        //     </div> 
+        // </div> 
 
 export default More
