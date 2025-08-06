58c95bc5bda733938f4efac32cbb4ea78cbb5b22
badger-dowdeswell <badger@rockweather.com>
2025-08-06 15:59:30 +1200

    sync

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-   ],
-   theme: {
-      // Placing the custom colour specification inside
-      // the extend section preserves all the default
-      // theme colours.
-      extend: {
-         colors: {
-            "twitter-blue": "#1DA1F2",
-         },
-      },
-   },
-   plugins: [],
-}
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    // Placing the custom colour specification inside
+    // the extend section preserves all the default
+    // theme colours.
+    extend: {
+        colors: {
+                "twitter-blue": "#1DA1F2",
+            },
+        },
+    },
+    plugins: [
+        require("tailwind-scrollbar-hide")
+    ],
+};
